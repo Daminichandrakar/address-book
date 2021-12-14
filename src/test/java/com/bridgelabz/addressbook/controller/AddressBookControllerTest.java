bc2cd1018c65f45c3f2c27ae0d37192f36ca37fb
Damini <damini.2201@gmail.com>
2021-12-15 03:48:33 +0530

    Added : Test case for controller class to check the method to add addressBook details

--- src/test/java/com/bridgelabz/addressbook/controller/AddressBookControllerTest.java
+++ src/test/java/com/bridgelabz/addressbook/controller/AddressBookControllerTest.java
@@ -12,6 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,22 +27,22 @@ public class AddressBookControllerTest {
     @Test
     void given2AddressBookDto_whenCalledGetAllMethod_shouldReturnListOfAddressBookDto() {
         List<AddressBookDto> addressBookDtoList = new ArrayList<>();
-        AddressBookDto addressDto = new AddressBookDto();
-        addressDto.setName("Damini");
-        addressDto.setAddress("Mahasamund");
-        addressDto.setCity("Raipur");
-        addressDto.setState("Chhattishgarh");
-        addressDto.setPhoneNumber("1234567890");
-        addressDto.setZip("123456");
-        addressBookDtoList.add(addressDto);
-        AddressBookDto addressDto2 = new AddressBookDto();
-        addressDto2.setName("Siva");
-        addressDto2.setAddress("Mahasamund");
-        addressDto2.setCity("Raipur");
-        addressDto2.setState("Chhattishgarh");
-        addressDto2.setPhoneNumber("1234567890");
-        addressDto2.setZip("123456");
-        addressBookDtoList.add(addressDto2);
+        AddressBookDto addressBookDto = new AddressBookDto();
+        addressBookDto.setName("Damini");
+        addressBookDto.setAddress("Mahasamund");
+        addressBookDto.setCity("Raipur");
+        addressBookDto.setState("Chhattishgarh");
+        addressBookDto.setPhoneNumber("1234567890");
+        addressBookDto.setZip("123456");
+        addressBookDtoList.add(addressBookDto);
+        AddressBookDto addressBookDto2 = new AddressBookDto();
+        addressBookDto2.setName("Siva");
+        addressBookDto2.setAddress("Mahasamund");
+        addressBookDto2.setCity("Raipur");
+        addressBookDto2.setState("Chhattishgarh");
+        addressBookDto2.setPhoneNumber("1234567890");
+        addressBookDto2.setZip("123456");
+        addressBookDtoList.add(addressBookDto2);
 
         when(addressBookService.getAll()).thenReturn(addressBookDtoList);
         List<AddressBookDto> actualResponse = addressBookController.getAll();
@@ -53,6 +54,21 @@ public class AddressBookControllerTest {
             Assertions.assertEquals(addressBookDtoList.get(i).getPhoneNumber(), actualResponse.get(i).getPhoneNumber());
             Assertions.assertEquals(addressBookDtoList.get(i).getZip(), actualResponse.get(i).getZip());
         }
-
     }
+
+    @Test
+    void givenAddressBookDto_whenCalledAddAddressBookMethod_shouldReturnSuccessMessage() {
+        String successString = "AddressBook Added Successfully";
+        AddressBookDto addressBookDto = new AddressBookDto();
+        addressBookDto.setName("Damini");
+        addressBookDto.setAddress("Mahasamund");
+        addressBookDto.setCity("Raipur");
+        addressBookDto.setState("Chhattishgarh");
+        addressBookDto.setPhoneNumber("1234567890");
+        addressBookDto.setZip("123456");
+        when(addressBookService.addAddressBook(addressBookDto)).thenReturn(successString);
+        String actualResponseString = addressBookController.addEmployee(addressBookDto);
+        assertEquals(successString, actualResponseString);
+    }
+
 }
