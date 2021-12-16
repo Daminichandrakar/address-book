aa00389c8e14e885e1f4ae130fa4eb4a9c125c30
Damini <damini.2201@gmail.com>
2021-12-16 17:31:57 +0530

    Added : Integration testing for controller class to getAll address fron addressBook application

---
+++ src/test/java/com/bridgelabz/addressbook/integration/AddressBookControllerIT.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.addressbook.integration;
+
+import com.bridgelabz.addressbook.controller.AddressBookController;
+import com.bridgelabz.addressbook.service.AddressBookService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(AddressBookController.class)
+public class AddressBookControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private AddressBookService addressBookService;
+
+    @Test
+    void getAllAddressBookTest() throws Exception {
+        when(addressBookService.getAll()).thenReturn(new ArrayList<>());
+        mockMvc.perform(MockMvcRequestBuilders.get("/addressbook/get-all"))
+                .andExpect(status().isOk());
+    }
+
+}
